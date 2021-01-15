b39efb582f8cdeed3edfc1ad7e889167a847373d
le-garff-yoann <yoann.legarff@naval-group.com>
2021-01-15 09:34:27 +0100

    fix(refresh.sh) docker build -f Dockerfile ...

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t local/monero-miner .
+docker build -f Dockerfile -t local/monero-miner .
 
 docker rm -f monero-miner 2>/dev/null || echo "No running container to delete"
 docker run -d --name monero-miner local/monero-miner $@
