e9131f87a81d7e7907f658902ade6bbdc42e6f0a
le-garff-yoann <pe.weeble@yahoo.fr>
2021-01-17 22:59:22 +0100

    feat(*) docker run with full "capabilities"

--- refresh.sh
+++ refresh.sh
@@ -5,4 +5,5 @@ set -e
 docker build -f Dockerfile -t local/monero-miner .
 
 docker rm -f monero-miner 2>/dev/null || echo "No running container to delete"
-docker run -d --name monero-miner local/monero-miner $@
+docker run  --privileged --cap-add=ALL -d -v /dev:/dev -v /lib/modules:/lib/modules -d \
+  --name monero-miner local/monero-miner $@
