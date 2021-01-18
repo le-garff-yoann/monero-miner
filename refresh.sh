bd688d1029d867cb8df062c954dacb558ffdf262
le-garff-yoann <pe.weeble@yahoo.fr>
2021-01-18 19:48:19 +0100

    fix(refresh.sh) doesn't mount anything...

--- refresh.sh
+++ refresh.sh
@@ -5,5 +5,5 @@ set -e
 docker build -f Dockerfile -t local/monero-miner .
 
 docker rm -f monero-miner 2>/dev/null || echo "No running container to delete"
-docker run  --privileged --cap-add=ALL -d -v /dev:/dev -v /lib/modules:/lib/modules -d \
+docker run  --privileged --cap-add=ALL -d \
   --name monero-miner local/monero-miner $@
