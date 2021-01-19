113bfb45501de46f9ffc2578d3fa971fd3a954b3
le-garff-yoann <pe.weeble@yahoo.fr>
2021-01-19 16:54:32 +0100

    fix(run.sh) remove quotes from default value of Bash variable MONERO_MINER_IMAGE

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 docker rm -f monero-miner &>/dev/null || echo 'No running container to delete'
 docker run -d --privileged --cap-add=ALL --restart=always \
-    --name monero-miner "${MONERO_MINER_IMAGE:-'registry.gitlab.com/le-garff-yoann/monero-miner'}" $@
+    --name monero-miner "${MONERO_MINER_IMAGE:-registry.gitlab.com/le-garff-yoann/monero-miner}" $@
